7c5a54668b2914687cbf77bce146c7c54418eacf
Restorationmichael4 <156471511+Restorationmichael4@users.noreply.github.com>
2025-02-20 21:38:06 +0100

    Update set.js

--- set.js
+++ set.js
@@ -16,7 +16,7 @@ global.ytdl = "https://ytdl.giftedtech.web.id"; // You Can Change this...
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "Gifted~Ze12jR6b#9fBRNnXSTQLnBHnb1Dok7a43i2TxO93zanJ6WARcCsM", // Add sess Id here espwcially when deploying on panels else use app.json and .env file...
 SUDO_NUMBERS: process.env.SUDO_NUMBERS || "2348083433738", //Add multiple Numbers with Country Codes without (+) Separated by Comma...
-ANTI_DELETE: process.env.ANTI_DELETE || "true", // Make true to use Antidelete for Chats, Groups, Statuses & Channels
+ANTI_DELETE: process.env.ANTI_DELETE || "false", // Make true to use Antidelete for Chats, Groups, Statuses & Channels
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || "false",
 AUTO_LIKE_EMOJI: process.env.AUTO_LIKE_EMOJI || "💜", //Input Yours custom...
@@ -29,9 +29,9 @@ PACK_AUTHOR: process.env.PACK_AUTHOR || "Titan", // Added // Input Yours custom.
 PACK_NAME: process.env.PACK_NAME || "Titan", // Added // Input Yours custom...
 PREFIX: process.env.PREFIX || ".",
 VERSION: process.env.VERSION || "5.0.0",
-ANTILINK: process.env.ANTILINK || "warn", //  Enter true to kick automatically or delete to delete without kicking or warn to warn before kicking
+ANTILINK: process.env.ANTILINK || "false", //  Enter true to kick automatically or delete to delete without kicking or warn to warn before kicking
 ANTICALL: process.env.ANTICALL || "true",
-ANTIBAD: process.env.ANTIBAD || "true",
+ANTIBAD: process.env.ANTIBAD || "false",
 BAD_WORDS: process.env.BAD_WORDS || "fuck, pussy, anus, idiot", // Add Yours Separated by Comma(will be deleted if ANTIBAD is set to true)
 ANTICALL_MSG: process.env.ANTICALL_MSG || "*_📞 Auto Call Reject Mode Active. 📵 No Calls Allowed!_*",
 AUTO_REACT: process.env.AUTO_REACT || "false",
@@ -40,7 +40,7 @@ BOT_PIC: process.env.BOT_PIC || "https://files.catbox.moe/rbhiqe.png", // You ca
 AUTO_AUDIO: process.env.AUTO_AUDIO || "false",
 AUTO_BIO: process.env.AUTO_BIO || "false",
 AUTO_BIO_QUOTE: process.env.AUTO_BIO_QUOTE || "Gifted creates things that creates other things",
-CHAT_BOT: process.env.CHAT_BOT || "group", // Put value to true to enablle for all chats only or inbox to ebanle in pm chats only or groups to enable in groups only else false
+CHAT_BOT: process.env.CHAT_BOT || "false", // Put value to true to enablle for all chats only or inbox to ebanle in pm chats only or groups to enable in groups only else false
 WELCOME: process.env.WELCOME || "false",
 GOODBYE: process.env.GOODBYE || "false",    
 AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "false", // Enter value to true for blueticking all messages, or commands for blueticking only commands else false
