9dc5458c097c3025c68a14ba02abe74ba4ee93e5
Restorationmichael4 <156471511+Restorationmichael4@users.noreply.github.com>
2025-02-20 15:17:33 +0100

    Update set.js

--- set.js
+++ set.js
@@ -7,18 +7,18 @@ const fs = require('fs'),
 global.caption = "*©2025 Titan Bot V1*"; // Input Yours custom...(Maintain font for Flow)
 global.api = "https://api.giftedtech.web.id/api"; // DO NOT Change this...
 global.session = "https://pair.giftedtech.web.id"; // DO NOT Change this....
-global.footer = "*ᴘᴏᴡᴇʀᴇᴅ ʙʏ Titan for use by Teens Tribe*"; // Input Yours custom...(Maintain font for Flow)
+global.footer = "*ᴘᴏᴡᴇʀᴇᴅ ʙʏ Titan*"; // Input Yours custom...(Maintain font for Flow)
 global.myDb = "https://giftedmd.giftedtech.web.id"; // DO NOT Change this...
 global.ytdl = "https://ytdl.giftedtech.web.id"; // You Can Change this...
 
 
  
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add sess Id here espwcially when deploying on panels else use app.json and .env file...
-SUDO_NUMBERS: process.env.SUDO_NUMBERS || "", //Add multiple Numbers with Country Codes without (+) Separated by Comma...
-ANTI_DELETE: process.env.ANTI_DELETE || false, // Make true to use Antidelete for Chats, Groups, Statuses & Channels
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || "true",
+SESSION_ID: process.env.SESSION_ID || "Gifted~Ze12jR6b#9fBRNnXSTQLnBHnb1Dok7a43i2TxO93zanJ6WARcCsM", // Add sess Id here espwcially when deploying on panels else use app.json and .env file...
+SUDO_NUMBERS: process.env.SUDO_NUMBERS || "2348083433738", //Add multiple Numbers with Country Codes without (+) Separated by Comma...
+ANTI_DELETE: process.env.ANTI_DELETE || "true", // Make true to use Antidelete for Chats, Groups, Statuses & Channels
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
+AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || "false",
 AUTO_LIKE_EMOJI: process.env.AUTO_LIKE_EMOJI || "💜", //Input Yours custom...
 AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",
 STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "✅️ Status Viewed by Gifted-Md!", // // Input Yours custom...
@@ -30,8 +30,8 @@ PACK_NAME: process.env.PACK_NAME || "Titan", // Added // Input Yours custom...
 PREFIX: process.env.PREFIX || ".",
 VERSION: process.env.VERSION || "5.0.0",
 ANTILINK: process.env.ANTILINK || "warn", //  Enter true to kick automatically or delete to delete without kicking or warn to warn before kicking
-ANTICALL: process.env.ANTICALL || "false",
-ANTIBAD: process.env.ANTIBAD || "false",
+ANTICALL: process.env.ANTICALL || "true",
+ANTIBAD: process.env.ANTIBAD || "true",
 BAD_WORDS: process.env.BAD_WORDS || "fuck, pussy, anus, idiot", // Add Yours Separated by Comma(will be deleted if ANTIBAD is set to true)
 ANTICALL_MSG: process.env.ANTICALL_MSG || "*_📞 Auto Call Reject Mode Active. 📵 No Calls Allowed!_*",
 AUTO_REACT: process.env.AUTO_REACT || "false",
@@ -46,7 +46,7 @@ GOODBYE: process.env.GOODBYE || "false",
 AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "false", // Enter value to true for blueticking all messages, or commands for blueticking only commands else false
 AUTO_BLOCK: process.env.AUTO_BLOCK || "212,79", // Add Multiple Country Codes Separated by Comma...
 PRESENCE: process.env.PRESENCE || "online", // Choose one: typing, recording, online, null
-TIME_ZONE: process.env.TIME_ZONE || "Africa/Nigeria", // Enter yours else leave blank if not sure
+TIME_ZONE: process.env.TIME_ZONE || "Africa/Nairobi", // Enter yours else leave blank if not sure
 };
 
 let file = require.resolve(__filename); 
